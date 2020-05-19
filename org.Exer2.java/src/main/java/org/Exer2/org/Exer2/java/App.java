9a7eb5a2c8424a2cf5b71b9da95e49d5578bc0ef
NandhiniJM <65615418+NandhiniJM@users.noreply.github.com>
2020-05-19 22:00:44 +0530

    Add files via upload

---
+++ org.Exer2.java/src/main/java/org/Exer2/org/Exer2/java/App.java
@@ -0,0 +1,13 @@
+package org.Exer2.org.Exer2.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ org.Exer2.java/src/main/java/org/Exer2/org/Exer2/java/Calculator.java
@@ -0,0 +1,26 @@
+package org.Exer2.org.Exer2.java;
+
+public class Calculator {
+	private int num1,num2;
+
+	public int getNum1() {
+		return num1;
+	}
+
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+
+	public int getNum2() {
+		return num2;
+	}
+
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	
+	public void sum(int n1,int n2 )
+	{
+		System.out.println(n1+n2);
+	}
+}
