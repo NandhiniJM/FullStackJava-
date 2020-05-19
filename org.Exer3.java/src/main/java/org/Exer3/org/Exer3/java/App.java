b164b11d3edc67c56a92585e2ba1b0152bbd0a03
NandhiniJM <65615418+NandhiniJM@users.noreply.github.com>
2020-05-19 22:06:36 +0530

    Add files via upload

---
+++ org.Exer3.java/src/main/java/org/Exer3/org/Exer3/java/App.java
@@ -0,0 +1,16 @@
+package org.Exer3.org.Exer3.java;
+
+import org.Exer2.org.Exer2.java.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Calculator c= new Calculator();
+        c.sum(2, 3);
+    }
+}
---
+++ org.Exer3.java/src/test/java/org/Exer3/org/Exer3/java/AppTest.java
@@ -0,0 +1,20 @@
+package org.Exer3.org.Exer3.java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
